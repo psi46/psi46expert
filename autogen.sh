1718e4dca4208be88a9479cbc713a68c3b7dd2e1
rossini <rossini@4525493e-7705-40b1-a816-d608a930855b>
2011-11-16 16:38:59 +0000

    Renamed the configure.ac file to conf.ac.orig such that it does not change when running autogen.sh. The script autogen.sh was adapted such that it creates the configure.ac file.
    git-svn-id: https://svn.cern.ch/reps/psi46expert/trunk@17 4525493e-7705-40b1-a816-d608a930855b

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Copy the original file to have the normal name
+cp conf.ac.orig configure.ac
+
+# Update the file if necessary
 autoupdate
+
+# Remove the copy the updating program leaves
+rm -f configure.ac~
+
+# Reconfigure
 autoreconf --install
