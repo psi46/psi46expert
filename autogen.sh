2954e8e44438db15d5958f194a168d27b0731eb2
rossini <rossini@4525493e-7705-40b1-a816-d608a930855b>
2011-11-17 10:12:05 +0000

    Improved the autogen.sh script such that it will check for programs before running the actual commands. This will provide user friendly error messages, telling what is missing.
    git-svn-id: https://svn.cern.ch/reps/psi46expert/trunk@18 4525493e-7705-40b1-a816-d608a930855b

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Function that checks for program availability (works only
+# with programs the accept the --version argument)
+function check_prog {
+	# call the program with the --version argument
+	# redirect any output to /dev/null
+	$1 --version > /dev/null 2>&1
+	
+	# if it doesn't exist, you should get an error status
+	if [[ $? != 0 ]]
+	then
+		echo "Error: program '$1' missing. Please install it."
+		echo "Trying to continue ..."
+	fi
+}
+
+# Check programs which are required for the next steps.
+# This is for user friendliness only.
+check_prog autoconf
+check_prog automake
+check_prog autoreconf
+check_prog autoupdate
+check_prog libtool
+
 # Copy the original file to have the normal name
 cp conf.ac.orig configure.ac
 
