c23c3cd536929b993292e95bccb5817765dea3c0
rossini <rossini@4525493e-7705-40b1-a816-d608a930855b>
2011-12-02 17:50:17 +0000

    Updated the INSTALL file and improved the autogen.sh script.
    git-svn-id: https://svn.cern.ch/reps/psi46expert/trunk@25 4525493e-7705-40b1-a816-d608a930855b

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,9 @@
 # Function that checks for program availability (works only
 # with programs the accept the --version argument)
 function check_prog {
-	# call the program with the --version argument
-	# redirect any output to /dev/null
-	$1 --version > /dev/null 2>&1
+	# Try finding the program with the 'which' command.
+	# Redirect any output to /dev/null.
+	which $1 > /dev/null 2>&1
 	
 	# if it doesn't exist, you should get an error status
 	if [[ $? != 0 ]]
@@ -19,8 +19,6 @@ function check_prog {
 # This is for user friendliness only.
 check_prog autoconf
 check_prog automake
-check_prog autoreconf
-check_prog autoupdate
 check_prog libtool
 
 # Copy the original file to have the normal name
