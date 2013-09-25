0e67737fe887e7cc266b141fcf562eb29bdb7330
philippeller <peller@phys.ethz.ch>
2013-09-25 11:01:38 +0200

    fixed PH=-1 in fitter

--- src/psi46expert/PHCalibrationFit.cc
+++ src/psi46expert/PHCalibrationFit.cc
@@ -261,7 +261,7 @@ void PHCalibrationFit::FitAllCurves(char * dirName, int nRocs)
 
                     if (1 == fitMode || 3 == fitMode)
                     {
-                        if (strcmp(string, "N/A") == 0);
+                        if (!(atoi(string)<0));
                         else {
                             ph[i] = atoi(string);
                             x[n] = (double)ph[i];
@@ -271,7 +271,7 @@ void PHCalibrationFit::FitAllCurves(char * dirName, int nRocs)
                     }
                     if (0 == fitMode)
                     {
-                        if ((strcmp(string, "N/A") == 0) || (i < 2) || (i > 2 * vcalSteps - 2));
+                        if (!(atoi(string)<0) || (i < 2) || (i > 2 * vcalSteps - 2));
                         else {
                             ph[i] = atoi(string);
                             x[n] = (double)ph[i];
@@ -339,7 +339,7 @@ void PHCalibrationFit::FitCurve(char * dirName, int chip, int col, int row)
         fscanf(inputFile, "%s", string);
         if (1 == fitMode || 3 == fitMode)
         {
-            if (strcmp(string, "N/A") == 0);
+            if (!(atoi(string)<0));
             else {
                 ph[i] = atoi(string);
                 printf("ph %i vcal %.0f\n", ph[i], vcalLow[i]);
@@ -350,7 +350,7 @@ void PHCalibrationFit::FitCurve(char * dirName, int chip, int col, int row)
         }
         if (0 == fitMode)
         {
-            if ((strcmp(string, "N/A") == 0) || (i < 2) || (i > 2 * vcalSteps - 2));
+            if (!(atoi(string)<0) || (i < 2) || (i > 2 * vcalSteps - 2));
             else {
                 ph[i] = atoi(string);
                 //              printf("ph %i vcal %.0f\n", ph[i], vcalLow[i]);
