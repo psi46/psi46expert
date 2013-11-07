68ec279a58428ba8c4f4a286e66ab08da314738f
Hanno Perrey <hanno.perrey@desy.de>
2013-11-07 17:05:48 +0100

    merged modifications to takeData and daqFrame made at DESY (stored locally on machine h1alexey); please note: not included were various delays (most of which are likely obsolete) and the hard-coded setting of the WBC parameter; instead the one set in the config is used.

--- src/psi46expert/takeData.cpp
+++ src/psi46expert/takeData.cpp
@@ -11,17 +11,51 @@
 #include "BasePixel/DecoderCalibration.h"
 #include "BasePixel/DecodedReadout.h"
 #include "BasePixel/ConfigParameters.h"
+#include <stdlib.h>
 #include <TString.h>
 #include <TApplication.h>
+#include <signal.h> // for handling ctrl-c (sigint) events
+
+daqFrame *dF; // need global daqFrame object so that we can refer to it from signal handling function
+int caught_sigint;
+
+void my_sigint_handler(int s){
+  if (s != 2) {
+    printf("Caught signal %d, not handled by this routine\n",s);
+    return;
+  }
+  if (dF == 0 || caught_sigint>4){
+    // object not ready yet or already caught sigint: just exit
+    printf("Caught signal %d, going to exit hard\n",s);
+    exit(1); 
+  }
+  else {
+    printf("Caught signal %d, going to stop run\n",s);
+    caught_sigint++;
+    dF->doStop();
+  }
+}
 
 
 // ----------------------------------------------------------------------
 int main(int argc, char * argv[])
 {
-    int mode(7), runnumber(0), localtrigger(0);
-    bool batchMode = false, trimArg = false, dacArg = false, maskArg = false;
-    char rootFile[1000], logFile[1000], dacFile[1000], trimFile[1000], directory[1000], tbName[1000], maskFile[1000];
-
+  // init (global) daqFrame object 
+  dF = 0;
+  caught_sigint = false;
+  // setup signal handling
+  struct sigaction act;
+  act.sa_handler = my_sigint_handler;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  sigaction(SIGINT, &act, 0);
+  
+  int mode(7), runnumber(0), localtrigger(0);
+  int secondBoard = 0;
+  int duration = -1;
+  bool batchMode = false, trimArg = false, dacArg = false, maskArg = false;	
+  char rootFile[1000], logFile[1000], dacFile[1000], trimFile[1000], directory[1000], tbName[1000], maskFile[1000];
+  int numROCs = 1;
 
     ConfigParameters * mtbConfigParameters = new ConfigParameters();
     //  sprintf(mtbConfigParameters->directory, "testModule");
@@ -34,8 +68,12 @@ int main(int argc, char * argv[])
     for (int i = 0; i < argc; i++)  {
         if (!strcmp(argv[i], "-b")) batchMode = true;
         if (!strcmp(argv[i], "-V")) V = atoi(argv[++i]);
+        if (!strcmp(argv[i], "-numrocs")) numROCs = atoi(argv[++i]);
         if (!strcmp(argv[i], "-l")) localtrigger = 1;
-        if (!strcmp(argv[i], "-m")) mode = atoi(argv[++i]);
+	if (!strcmp(argv[i],"-s")) secondBoard = 1;
+	if (!strcmp(argv[i], "-m")) mode = atoi(argv[++i]);
+	if (!strcmp(argv[i],"-r")) runnumber = atoi(argv[++i]);
+	if (!strcmp(argv[i],"-duration")) duration = atoi(argv[++i]);
         if (!strcmp(argv[i], "-dir")) strcpy(mtbConfigParameters->directory, argv[++i]);
         if (!strcmp(argv[i], "-trimVcal"))
         {
@@ -48,7 +86,8 @@ int main(int argc, char * argv[])
         if (!strcmp(argv[i], "-mask"))
         {
             maskArg = true;
-            sprintf(maskFile, "%s", "pixelMask.dat");
+	    sprintf(maskFile, "%s/%s", mtbConfigParameters->directory, "pixelMask.dat");
+
         }
     }
 
@@ -58,20 +97,23 @@ int main(int argc, char * argv[])
     if (maskArg) mtbConfigParameters->SetMaskFileName(maskFile);
 
     //logging manager
-    daqLoggingManager * lm = new daqLoggingManager("/tmp");
+    daqLoggingManager * lm = new daqLoggingManager("../data");
     lm->setRunMode(mode);
     lm->setMTBConfigParameters(mtbConfigParameters);
+
+    if (runnumber > 0) lm->setRunNumber(runnumber);
+    else if( secondBoard ) runnumber = lm->incrementRunNumber();
 
     //decoder
     RawPacketDecoder * gDecoder = RawPacketDecoder::Singleton();
     TString fileName = TString(mtbConfigParameters->directory).Append("/addressParameters.dat");
     cout << "Reading Address Level-Parameters from " << fileName << endl;
-    DecoderCalibrationModule * decoderCalibrationModule = new DecoderCalibrationModule(fileName, 3, 0, DecodedReadoutConstants::NUM_ROCSMODULE);
+    //DecoderCalibrationModule * decoderCalibrationModule = new DecoderCalibrationModule(fileName, 3, 0, DecodedReadoutConstants::NUM_ROCSMODULE);
+    DecoderCalibrationModule * decoderCalibrationModule = new DecoderCalibrationModule(fileName, 3, 0, numROCs);
     decoderCalibrationModule->Print(&cout);
     gDecoder->SetCalibration(decoderCalibrationModule);
 
     UsbDaq * daq = new UsbDaq(mode);
-    if (runnumber > 0) lm->setRunNumber(runnumber);
 
     histogrammer * h = new histogrammer();
     daq->setHistogrammer(h);
@@ -79,19 +121,19 @@ int main(int argc, char * argv[])
 
     // -- GUI
     TApplication * application = new TApplication("App", 0, 0, 0, -1);
-    daqFrame * dF = new daqFrame(gClient->GetRoot(), 1000, 700, lm, batchMode);
+  dF = new daqFrame(gClient->GetRoot(), 1000, 700, lm, batchMode);
     dF->setUsbDAQ(daq);
     dF->setLoggingManager(lm);
     if (localtrigger) dF->fLocalTrigger = 1;
     //  if(V>0)dF->doVup(V);
 
+    if( duration > 0 ) dF->setRunDuration(duration);
+
     if (batchMode) {
-        dF->setRunDuration(15);
         dF->setFillMem(0);
         dF->doSetSysCommand1Text(); //exec module.ini
         if (V > 0)dF->doVup(V);
         dF->doStart();
-        dF->doStop();
         if (V > 0)dF->doVdown(V);
         dF->doExit();
     } else  {
